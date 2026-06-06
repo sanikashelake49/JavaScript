d97da48bb9022077029a82fc543f90e062d95565
Sanika <shelakesanika370@gmail.com>
2026-06-06 14:31:57 +0530

    simple array method uses in java script

---
+++ JAVA_SCRIPT PRACTICE/Array/array.js
@@ -0,0 +1,68 @@
+let product=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+//product.shift(1);
+
+
+
+
+
+ 
+
+//discount   
+// let item=[250,645,300,900,50];
+
+// let i=0;
+// for(let val of item){
+//     console.log(`value at index ${i} = ${val}`);
+    
+//     let offer=val/10;
+//     item[i]=item[i]-offer;
+
+//     console.log(`value after offer =${item[i]}`);
+//     i++;
+// }
+ 
+// for(let i=0;i<item.length;i++){
+//     console.log(`value ${item[i]}`);
+//     let offer=item[i]/10;
+//         item[i]-=offer;
+//         console.log(`value after offer ${item[i]}`);
+
+// }
+  
+// //finding avr
+// let mark=[85,97,44,37,76,60];
+// let arr=[];
+
+// for(let i=0;i<mark.length;i++){
+// arr.push(mark[i]);
+
+// }
+
+// console.log(arr);
+// let sum=0;
+// for(let i=0;i<mark.length;i++){
+//     sum+=mark[i];
+// }
+
+// let avg=sum/mark.length;
+
+// console.log("avrage of the class student is ",avg);
+
+
+
+
+// let arr=[1,2,3,4,5];
+
+// console.log(arr); 
+
+// let lang=["java","python","sql","JS"];
+
+// for(let i=0;i<lang.length;i++){
+//    console.log(lang[i]);    
+
+// } 
+
+// for(let info of lang){
+//  console.log(info.toUpperCase( ));
+
+// }
